b4f6ff6c1bddb7f237342e1b11a1e918551d0197
Leong <71641194+Leong077@users.noreply.github.com>
2020-12-16 02:56:45 +0800

    代码优化及完成commit类
    学长您好，以下是本次任务的设计文档：
    GenIniTree类：
    对文件路径使用深度优先搜索方法来构造初始树，遇到文件则储存为blob对象并加入Tree中，遇到文件夹则存储为Tree对象并加入上一层Tree中，并进行递归。构造结束后，再生成Tree的key-value文件。
    
    Tree类：
    暂时还是保留了用逐个add算hash的方法来构造初始树。之后为了提高效率，会改成先建成整个树，再依次计算每个tree节点的hash。
    
    Commit类：
    对于commit，增加了一个rootsha的属性来记录所对应根的hash值，方便与新的commit进行对比，如果相等，则不运行生成文件的函数。
    
    此外还优化了上次任务的类的设计，实现了keyvalue的存储。
    麻烦学长看看有没有进一步改进的地方，谢谢！ @js00070

---
+++ CalHash.java
@@ -0,0 +1,64 @@
+package javagit;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+public class CalHash {
+	
+	 public static byte[] SHA1Checksum(String content) throws Exception {
+        MessageDigest complete = MessageDigest.getInstance("SHA-1");
+        complete.update(content.getBytes());
+        return complete.digest();
+	 }
+	 
+	 public static byte[] SHA1Checksum(InputStream is) throws Exception {
+		 byte[] buffer = new byte[1024];
+	        MessageDigest complete = MessageDigest.getInstance("SHA-1");
+	        int numRead = 0;
+	        do {
+	            numRead = is.read(buffer);
+	            if(numRead > 0){
+	                complete.update(buffer, 0, numRead);
+	            }
+	        }while (numRead != -1);
+	        is.close();
+	        return complete.digest();
+	 }
+	 
+	 //计算单个Blob对象哈希值的静态方法
+	 public static String blobHash(String filepath) throws Exception {
+		File file = new File(filepath);
+		FileInputStream is = new FileInputStream(file);
+	    byte[] sha1 = SHA1Checksum(is);
+	    String result = "";
+	    for (int i = 0; i < sha1.length; i++) {
+	        String append = Integer.toString(sha1[i]&0xFF, 16);
+	        if (append.length() < 2) {
+	    	result = result + "0" + append;
+	        } else {
+	        	result += append;
+	        }
+	    }
+	    return result;
+	 }
+	 
+	 //计算tree和commit哈希值的静态方法
+	 public static String struHash(String content) throws Exception {
+		byte[] sha1 = SHA1Checksum(content);
+		String result = "";
+		    for (int i = 0; i < sha1.length; i++) {
+		        String append = Integer.toString(sha1[i]&0xFF, 16);
+		        if (append.length() < 2) {
+		    	result = result + "0" + append;
+		        } else {
+		        	result += append;
+		        }
+		    }
+	    return result;
+	 }
+}
--- Tree.java
+++ Tree.java
@@ -1,41 +1,25 @@
 package javagit;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import javagit.hash;
+import javagit.CalHash;
 
 public class Tree {
 	private String sha;
-	private ArrayList<String> fname;
-	private ArrayList<String> fhash;
+	private String content;
 	
 	public Tree() {}
 	
-	public void addblob(Blob b, String name){
-		fname.add(name);
-		fhash.add(b.getSha());
-		String s = "";
-		for(int i = 0; i < fname.size(); i++) {
-			s += fname.get(i);
-		}
-		for(int i = 0; i < fhash.size(); i++) {
-			s += fhash.get(i);
-		}
-		hash hs = new hash();
-		setSha(hs.stringhash(s));
+	public void addblob(Blob b, String name) throws Exception{
+		setContent(getContent() + "blob" + b.getSha() + name + "/n");
+		setSha(CalHash.struHash(getContent()));
 	}
 	
 	public void addtree(Tree t, String name) throws Exception {
-		fname.add(name);
-		fhash.add(t.getSha());
-		String s = "";
-		for(int i = 0; i < fname.size(); i++) {
-			s += fname.get(i);
-		}
-		for(int i = 0; i < fhash.size(); i++) {
-			s += fhash.get(i);
-		}
-		hash hs = new hash();
-		setSha(hs.stringhash(s));
+		setContent(getContent() + "tree" + t.getSha() + name + "/n");
+		setSha(CalHash.struHash(getContent()));
 	}
 
 	public String getSha() {
@@ -45,5 +29,20 @@ public class Tree {
 	public void setSha(String sha) {
 		this.sha = sha;
 	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 	
+	public void genTreeFile() throws Exception {
+		File f = new File(".\\object\\");
+		Tree t = new Tree();
+		f.createNewFile();
+		FileWriter newFile = new FileWriter(f);
+		newFile.write(t.getContent());
+	}
 }
