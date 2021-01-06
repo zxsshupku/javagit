da5a6a77087d1f32963b5cdf3f19eabf7b5effa5
zxsshupku <72910705+zxsshupku@users.noreply.github.com>
2021-01-06 23:43:09 +0800

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -2,18 +2,22 @@ public class Tree {
 	private String sha;
 	private String content;
 	
-	public Tree() {
-		content = " ";
-	}
+	public Tree() {}
 	
 	public void addblob(Blob b, String name) throws Exception{
-		//为了方便解析取消了换行符
-		setContent(getContent() + " blob " + b.getSha() + " " + name + " ");
+		//涓轰簡鏂逛究瑙ｆ瀽鍙栨秷浜嗘崲琛岀
+		if(getContent().equals(null))
+			setContent("blob " + b.getSha() + " " + name);
+		else
+			setContent(getContent() + " blob " + b.getSha() + " " + name);
 		setSha(CalHash.struHash(getContent()));
 	}
 	
 	public void addtree(Tree t, String name) throws Exception {
-		setContent(getContent() + " tree " + t.getSha() + " " + name + " ");
+		if(getContent().equals(null))
+			setContent("tree " + t.getSha() + " " + name);
+		else
+			setContent(getContent() + " tree " + t.getSha() + " " + name);
 		setSha(CalHash.struHash(getContent()));
 	}
 
